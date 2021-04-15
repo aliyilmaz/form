cdc2a0fcb0bd6944af483a87033a5a5fd5fab809
aliyilmaz <aliyilmaz.work@gmail.com>
2021-04-15 14:55:16 +0300

    Automatic creation of folders was achieved.

--- app/api/form.php
+++ app/api/form.php
@@ -1,11 +1,14 @@
 <?php
 $response = [];
 
+
 if(isset($this->post['files'])){
+    if(!is_dir('public/files')){mkdir('public/files'); }
     $response = $this->upload($this->post['files'], 'public/files');
 }
 
 if(isset($this->post['file'])){
+    if(!is_dir('public/file')){ mkdir('public/file'); }
     $response = $this->upload($this->post['file'], 'public/file');
 }
 
